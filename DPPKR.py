dd7416e2492a1e1832e9bdf1cd438167af6e8d1b
hakimmokhtar <hakimmokhtar1998@gmail.com>
2025-06-02 23:16:14 +0800

    Update DPPKR.py

--- DPPKR.py
+++ DPPKR.py
@@ -5,8 +5,12 @@ import pandas as pd
 sheet_id = "1qJmyiXVzcmzcfreSdDC1cV0Hr4iVsQcA99On-0NPOck"
 sheet_url = f"https://docs.google.com/spreadsheets/d/1qJmyiXVzcmzcfreSdDC1cV0Hr4iVsQcA99On-0NPOck/export?format=csv"
 
-df = pd.read_csv(sheet_url)
+@st.cache_data
+def load_data():
+    sheet_id = "GANTI_DENGAN_ID_SHEET"
+    sheet_url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv"
+    df = pd.read_csv(sheet_url, header=1)
+    df['Tarikh'] = pd.to_datetime(df['Tarikh'], dayfirst=True)
+    return df.sort_values('Tarikh')
 
-# 👇 Ini debug penting
-st.write("📋 Kolum yang dibaca dari Google Sheet:", df.columns)
-st.write("🔍 Data awal yang dibaca:", df.head())
+
