ca612e24de9e202f9a75e81f2139e03d029ea304
hakimmokhtar <hakimmokhtar1998@gmail.com>
2025-06-03 14:06:32 +0800

    Update DPPKR.py

--- DPPKR.py
+++ DPPKR.py
@@ -125,7 +125,7 @@ footer_html = """
     <span>&#128231;</span> <a href="mailto:setiausaha@dppkr.my">setiausaha@dppkr.my</a> |
     <span>&#128279;</span> <a href="https://facebook.com/pemudapasrembau" target="_blank">Facebook </a><br>
     <span>&#128222;</span> SU : <a href="tel:+60136343231">+6013634323</a> |
-    <span>&#128172;</span> <a href="https://wa.me/60123456789" target="_blank">WhatsApp</a><br>
+    <span>&#128172;</span> <a href="https://wa.me/60136343231" target="_blank">WhatsApp</a><br>
     <span>&#128222;</span> SU 2: <a href="tel:+60198765432">+60 19-876 5432</a> |
     <span>&#128172;</span> <a href="https://wa.me/60198765432" target="_blank">WhatsApp</a>
 </div>
